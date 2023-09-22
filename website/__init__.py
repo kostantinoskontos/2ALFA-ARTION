fd8ae37e30459eaa8219f8f03f3b6753398f22b3
konstantinoskontos <kostas.jenious@hotmail.com>
2023-09-22 19:34:06 +0300

    message

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 
-def create_app():
 
+def create_app():
     app = Flask(__name__)
     app.config.from_object("config.DevelopmentConfig")
 
-
     from .views import views
-        
-    app.register_blueprint(views, url_prefix='/')
+
+    app.register_blueprint(views, url_prefix="/")
 
     return app
-
-   
